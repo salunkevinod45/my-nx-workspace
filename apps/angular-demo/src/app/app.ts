e017eddde4c46e7d9f43f042485f165e2eb81c58
Vinod Salunke <Vinod.Salunke@vanderlande.com>
2026-01-16 14:25:11 +0530

    add hero component

--- apps/angular-demo/src/app/app.ts
+++ apps/angular-demo/src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Hero } from '@angular-demo/ui';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule,Hero],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-demo';
+
+  ctaClick= () => {
+    alert('CTA button clicked!');
+  }
 }
